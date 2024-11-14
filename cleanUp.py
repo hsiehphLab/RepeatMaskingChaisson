0c2473c885bcaa76ec02f8b9980b313a83f9e362
David Gordon <gordo893@umn.edu>
2024-11-13 19:02:26 -0600

    cleanUp.py to try to debug why it isn't deleting all the files it should

--- cleanUp.py
+++ cleanUp.py
@@ -13,7 +13,8 @@ for szDirectory in aDirectoriesToMainlyEmpty:
     print( "now working on " + szDirectory )
 
     nFilesSoFar = 0
-
+    nFilesDeleted = 0
+    
     with os.scandir( szDirectory) as it:
         for entry in it:
             if not entry.name.startswith('.') and entry.is_file():
@@ -21,10 +22,11 @@ for szDirectory in aDirectoriesToMainlyEmpty:
                 if ( nFilesSoFar > nFilesToKeepInDirectories ):
                     #print( "about to delete: " + entry.name )
                     os.remove( szDirectory + "/" + entry.name )
+                    nFilesDeleted += 1
                 else:
                     print( "will keep: " + entry.name )
 
-
+    print( f"files deleted: {nFilesDeleted}" )
 
 
 # delete logs.  This saves each subdirectory of logs and saves 5 logs
